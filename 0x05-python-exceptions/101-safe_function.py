d15cef81ac070fd75e927642c6aecba362a5003d
amadasunese <117770810+amadasunese@users.noreply.github.com>
2023-03-28 14:26:01 +0100

    Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -18,6 +18,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except User.DoesNotExist:
+    except none:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
