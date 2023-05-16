91ba772dcb510a20ed4dac9fbc83b478d484762b
Ese <amadasunese@gmail.com>
2023-05-16 07:51:21 -0700

    sql task

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- Displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
 SELECT COUNT(*)
 FROM first_table 
-WHERE = 89;
+WHERE id = 89;
