77545b6960d7ba9313d1bda19ee4836b9a6eee1e
amadasunese <117770810+amadasunese@users.noreply.github.com>
2023-05-17 21:23:33 +0100

    sql more queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,8 +5,8 @@
 -- If the table cities already exists, your script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-	id INT AUTO_INCREMENT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
-);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       state_id INT NOT NULL,
+       FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+       name VARCHAR(256) NOT NULL);
