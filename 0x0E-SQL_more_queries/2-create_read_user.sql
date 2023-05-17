651ee3f803fbc20153ff4c3bcccbe38969ebda67
Ese <amadasunese@gmail.com>
2023-05-17 04:33:00 -0700

    SQL more queries task

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Create the database if it doesn't exist
+-- Create the user if it doesn't exist
+-- Grant SELECT privilege to the user for the hbtn_0d_2 database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
