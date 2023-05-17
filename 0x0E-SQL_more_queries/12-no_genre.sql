6fac548f8ffb1bb3628d6273b22f3ec7f51d6d83
amadasunese <117770810+amadasunese@users.noreply.github.com>
2023-05-17 21:30:47 +0100

    sql more queries

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,7 +2,6 @@ i-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download
 -- Lists all shows contained in hbtn_0d_tvshows without a genre linked.
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
--- You can use only one SELECT statement
 
 SELECT title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
