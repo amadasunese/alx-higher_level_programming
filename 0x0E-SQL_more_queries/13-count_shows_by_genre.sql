8c92d23b84c24a1bddd49c0220aa49301d19b355
amadasunese <117770810+amadasunese@users.noreply.github.com>
2023-05-17 20:14:06 +0100

    sql more queries

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,15 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 12-no_genre.sql)
+-- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+-- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
+-- First column must be called genre
+-- Second column must be called number_of_shows
+-- Don’t display a genre that doesn’t have any shows linked
+-- Results must be sorted in descending order by the number of shows linked
+-- You can use only one SELECT statement
+
+SELECT tv_genres.genre AS genre, COUNT(tv_shows.id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+GROUP BY tv_genres.id, tv_genres.genre
+ORDER BY number_of_shows DESC;
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+-- name VARCHAR(256) can’t be null
+-- If the table cities already exists, your script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+);
