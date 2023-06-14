9176822565803fcd76bd5c61a0f3b4539aa838fc
Ese Amadasun <amadasunese@gmail.com>
2023-06-14 14:54:19 +0000

    square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,12 +2,9 @@
 module.exports = class Square extends require('./5-quare.js') {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-
-    const row = c.repeat(this.size);
-    for (let i = 0; i < this.size; i++) {
-      console.log(row);
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
