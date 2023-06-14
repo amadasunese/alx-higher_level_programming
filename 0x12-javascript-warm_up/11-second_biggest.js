923baccd3e20246d367ddb865aa9f8e88076bf35
Ese Amadasun <amadasunese@gmail.com>
2023-06-14 08:11:02 +0000

    fix errors

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,27 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 3) {
+// Check if there are any arguments
+if (process.argv.length <= 2) {
+  console.log(0);
+} else if (process.argv.length === 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  // Convert arguments to integers
+  const numbers = process.argv.slice(2).map(Number);
+
+  // Find the second largest integer
+  let largest = numbers[0];
+  let secondLargest = -Infinity;
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+      secondLargest = largest;
+      largest = numbers[i];
+    } else if (numbers[i] > secondLargest && numbers[i] < largest) {
+      secondLargest = numbers[i];
+    }
+  }
+
+  // Print the second largest integer
+  console.log(secondLargest);
 }
