9f527f6b066fdafee865517908b21b0c18175e02
Ese Amadasun <amadasunese@gmail.com>
2023-06-14 15:38:10 +0000

    occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-exports.nbOccurrences = function (list, searchElement) {
-  let count = 0;
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) {
-      count++;
-    }
-  }
-  return count;
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
 };
