5b2e03d936913db745a276faaea3eba305cb5128
amadasunese <117770810+amadasunese@users.noreply.github.com>
2023-06-15 14:28:50 +0100

    map

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,6 +3,6 @@
 const originalList = require('./100-data').list;
 console.log(originalList);
 const mappedList = originalList.map(function (e, index) {
-    return (e * index);
+  return (e * index);
 });
 console.log(mappedList);
