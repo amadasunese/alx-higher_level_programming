8a18d8f2a6103e8708f6b881c2b50e98e8523688
Ese <amadasunese@gmail.com>
2023-06-17 07:42:56 -0700

    mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,8 @@ from sys
 import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", 
+                        user=argv[1], passwd=argv[2], db=argv[3])
     query = "SELECT * FROM states\
              WHERE states.name LIKE BINARY '{}'\
              ORDER BY states.id ASC".format(argv[4])
