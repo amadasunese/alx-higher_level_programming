4096c7367b47fe2f9d2fa3092a5fd7ad9eeb186b
Ese <amadasunese@gmail.com>
2023-06-20 11:38:22 -0700

    fix pycodestyle error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,13 +5,14 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], db=argv[3])
     query = "SELECT cities.id, cities.name, states.name\
              FROM cities JOIN states\
              WHERE cities.state_id = states.id"
 
     cursor = db.cursor()
-    
+
     cursor.execute(query)
     for state in cursor.fetchall():
         print(state)
