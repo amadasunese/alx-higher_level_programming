1e73d387a51b8195b7f94760102d8439f347638e
Ese Amadasun <amadasunese@gmail.com>
2023-07-02 12:53:58 -0700

    fix error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,7 +4,7 @@ Python script that takes in a URL, sends a request to the URL
 and displays the body of the response.
 """
 import requests
-from sys import argv
+from sys
 
 
 if __name__ == "__main__":
