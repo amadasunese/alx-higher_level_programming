370dc0c1c7b9b2d2ee20e66d041f02b60ce86d36
Ese Amadasun <amadasunese@gmail.com>
2023-07-02 13:05:11 -0700

    fix error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,12 +4,21 @@ Python script that takes in a URL, sends a request to the URL
 and displays the body of the response.
 """
 import requests
-from sys import argv
+import sys
+
+
+def get_url_response(url):
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
+
 
 if __name__ == "__main__":
-    try:
-        res = requests.get(argv[1])
-        res.raise_for_status()
-        print(res.text)
-    else:
-        print("Error code: {}".format(res.status_code))
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    get_url_response(url)
