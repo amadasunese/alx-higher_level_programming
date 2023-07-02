d97b7d5b0fd9bc90bfec790d699205d6acccc180
Ese Amadasun <amadasunese@gmail.com>
2023-07-02 13:22:53 -0700

    update

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that takes your GitHub credentials 
+Python script that takes your GitHub credentials
 (username and password) and uses the GitHub API to display your id
 """
 import requests
@@ -12,5 +12,5 @@ if __name__ == "__main__":
         res = requests.get("https://api.github.com/user",
                            auth=(argv[1], argv[2])).json()
         print(res.get("id"))
-    except:
+    except ValueError:
         print("Not a valid PARAMETER")
